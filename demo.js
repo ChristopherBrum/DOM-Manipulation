e98004ad7d4b267289b47b95c79ef2fdfdbe2cad
Christopher Brum <christopher.brum@gmail.com>
2023-04-05 08:41:58 -0700

    updated the demo.js file

--- demo.js
+++ demo.js
@@ -66,10 +66,10 @@
 //   alert(`${this.className} ${e.currentTarget.tagName}`);
 // }
 
-let elem0 = document.querySelector('#elem0');
-let elem1 = document.querySelector('#elem1');
-let elem4 = document.querySelector('#elem4');
+// let elem0 = document.querySelector('#elem0');
+// let elem1 = document.querySelector('#elem1');
+// let elem4 = document.querySelector('#elem4');
 
-elem0.addEventListener('click', event => alert(event.currentTarget.id));
-elem1.addEventListener('click', event => alert(event.currentTarget.id), true);
-elem4.addEventListener('click', event => alert(event.currentTarget.id));
+// elem0.addEventListener('click', event => alert(event.currentTarget.id));
+// elem1.addEventListener('click', event => alert(event.currentTarget.id), true);
+// elem4.addEventListener('click', event => alert(event.currentTarget.id));

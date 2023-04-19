0b82139208410478da77cb4819d0319ca62bcf2c
Christopher Brum <christopher.brum@gmail.com>
2023-04-19 10:55:46 -0700

    updated index, demo, and styles

--- demo.js
+++ demo.js
@@ -847,6 +847,32 @@ Requirements:
 //   }
 // });
 
-function delegateEvent(parentElement, selector, eventType, callback) {
-  
-}
+document.addEventListener('DOMContentLoaded', () => {
+  let store = document.getElementById('store');
+
+  let request = new XMLHttpRequest();
+  request.open('GET', 'https://ls-230-web-store-demo.herokuapp.com/products');
+  // request.responseType = `json`;
+
+  request.addEventListener('error', (e) => {
+    console.log("event: ", e);
+    store.innerHTML = request.response
+  });
+  request.send();
+
+  store.addEventListener('submit', event => {
+    let form = event.target;
+
+    event.preventDefault();
+    let request = new XMLHttpRequest();
+
+    let data = new FormData(form);
+    // let data = request.response;
+
+    request.open('POST', `https://ls-230-web-store-demo.herokuapp.com${form.getAttribute('action')}`);
+    request.setRequestHeader('Authorization', 'token AUTH_TOKEN');
+
+    request.addEventListener('load', event => store.innerHTML = request.response);
+    request.send(data);
+  });
+});

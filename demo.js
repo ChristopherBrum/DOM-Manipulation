0f940e87d8d410df768ca4cbec8eb41b8b317649
Christopher Brum <christopher.brum@gmail.com>
2023-05-18 11:30:21 -0700

    updated demo.js

--- demo.js
+++ demo.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-	const div = document.querySelector('div');
+	// const promise = new Promise(function(resolve, reject) {
+	// 	const data = fetch('https://api.weather.gov/products');
+	// 	resolve(data);	
+	// });
 
-	function logEventTarget(event) {
-		console.log(event.target);
-	}
+	// promise
+	// 	.then((data) => console.log(data))
+	// 	.catch((err) => console.log(err))
+	// 	.finally(() => console.log("it's over now"));
+
+	const request = new XMLHttpRequest();
+	request.open('GET', 'https://api.weather.gov/points/39.7456,-97.0892');
 	
-	div.addEventListener('click', logEventTarget);
-})
+	document.addEventListener('load')
+});

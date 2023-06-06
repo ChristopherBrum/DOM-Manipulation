fec5d2a664f59c9946c412db25dfc55c41cf750e
Christopher Brum <christopher.brum@gmail.com>
2023-06-06 10:25:35 -0700

    added new booking app feature

---
+++ practice_projects/booking_app_node/public/javascripts/adding_schedules.js
@@ -0,0 +1,16 @@
+/*
+- "add more schedules" button will add another schedule template to the page
+- bottom of the page will have a "submit" button to submit the filled out schedules
+
+- a schedule will consist of 
+	- a numbered schedule title ("schedule 1", "schedule 2", etc)
+	- staf name (drop down)
+	- a date
+	- a time
+- inputs must all be filled in or an alert will be raised ("check your inputs")
+- if adding schedules is successful, raise alert "Schedules added"
+*/
+
+document.addEventListener('DOMContentLoaded', () => {
+	
+});

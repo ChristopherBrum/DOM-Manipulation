1f0d12f78c7261941b3bef5cc22d15d05ab5d186
Christopher Brum <christopher.brum@gmail.com>
2023-06-13 11:37:00 -0700

    updated the form validation feature use to OLOO pattern

--- Practice Problems/Form and Form Validation/javascripts/main.js
+++ Practice Problems/Form and Form Validation/javascripts/main.js
@@ -28,109 +28,191 @@ Implementation
 
 */
 
-function isEmptyInput(input, flashElement) {
-	if (input.trim().length < 1 && flashElement.id != 'phone-flash') {
-		flashElement.textContent = 'Input cannot be empty';
-		return true;
-	}
-}
+// function isEmptyInput(input, flashElement) {
+// 	if (input.trim().length < 1 && flashElement.id != 'phone-flash') {
+// 		flashElement.textContent = 'Input cannot be empty';
+// 		return true;
+// 	}
+// }
 
-function isInvalidEmail(email, flashElement) {
-	if (isEmptyInput(email, flashElement)) return true;
-	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-	if (!emailRegex.test(email)) {
-		flashElement.textContent = 'Email is incorrectly formatted';
-		return true;
-	}
-}
+// function isInvalidEmail(email, flashElement) {
+// 	if (isEmptyInput(email, flashElement)) return true;
+// 	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+// 	if (!emailRegex.test(email)) {
+// 		flashElement.textContent = 'Email is incorrectly formatted';
+// 		return true;
+// 	}
+// }
 
-function isInvalidPassword(password, flashElement) {
-	if (isEmptyInput(password, flashElement)) return;
-	if (password.length < 10) {
-		flashElement.textContent = 'Password must be at least 10 characters long';
-		return true;
-	}
-}
+// function isInvalidPassword(password, flashElement) {
+// 	if (isEmptyInput(password, flashElement)) return;
+// 	if (password.length < 10) {
+// 		flashElement.textContent = 'Password must be at least 10 characters long';
+// 		return true;
+// 	}
+// }
 
-function isInvalidPhone(phone, flashElement) {
-	if (phone.length === 0) return;
-	const phoneNumberRegex = /^\d{3}-\d{3}-\d{4}$/;
-	if (!phoneNumberRegex.test(phone)) {
-		flashElement.textContent = 'Phone number should be formatted: 123-456-7890';
-		return true;
-	}
-}
+// function isInvalidPhone(phone, flashElement) {
+// 	if (phone.length === 0) return;
+// 	const phoneNumberRegex = /^\d{3}-\d{3}-\d{4}$/;
+// 	if (!phoneNumberRegex.test(phone)) {
+// 		flashElement.textContent = 'Phone number should be formatted: 123-456-7890';
+// 		return true;
+// 	}
+// }
 
-function removeSubmitErrorMessage() {
-	const messageElement = document.getElementById('submit-error');
-	if (messageElement) messageElement.remove();
-}
+// function removeSubmitErrorMessage() {
+// 	const messageElement = document.getElementById('submit-error');
+// 	if (messageElement) messageElement.remove();
+// }
 
-function validateAllInputs() {
-	const inputs = document.querySelectorAll('input');
-	const validationMethods = [isEmptyInput, isEmptyInput, isInvalidEmail, isInvalidPassword, isInvalidPhone];
-	let invalidInputFound = false;
+// function validateAllInputs() {
+// 	const inputs = document.querySelectorAll('input');
+// 	const validationMethods = [isEmptyInput, isEmptyInput, isInvalidEmail, isInvalidPassword, isInvalidPhone];
+// 	let invalidInputFound = false;
 
-	inputs.forEach((input, index) => {
-		const flashId = input.id + '-flash';
-		const flashElement = document.getElementById(flashId);
+// 	inputs.forEach((input, index) => {
+// 		const flashId = input.id + '-flash';
+// 		const flashElement = document.getElementById(flashId);
 		
-		if (validationMethods[index](input.value, flashElement)) {
-			invalidInputFound = true;
-		}
-	});
+// 		if (validationMethods[index](input.value, flashElement)) {
+// 			invalidInputFound = true;
+// 		}
+// 	});
 
-	return invalidInputFound;
+// 	return invalidInputFound;
+// }
+
+const FormValidator = {
+	clearForm: function() {
+		const inputs = this.form.querySelectorAll('input');
+		inputs.forEach((input) => {
+			input.value = '';
+		});
+	},
+
+	isEmptyInput: function(input, flashElement) {
+		if (input.trim().length < 1 && flashElement.id != 'phone-flash') {
+			flashElement.textContent = 'Input cannot be empty';
+			return true;
+		}
+	},
+	
+	isInvalidEmail: function(email, flashElement) {
+		if (this.isEmptyInput(email, flashElement)) return true;
+		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		if (!emailRegex.test(email)) {
+			flashElement.textContent = 'Email is incorrectly formatted';
+			return true;
+		}
+	},
+	
+	isInvalidPassword: function(password, flashElement) {
+		if (this.isEmptyInput(password, flashElement)) return;
+		if (password.length < 10) {
+			flashElement.textContent = 'Password must be at least 10 characters long';
+			return true;
+		}
+	},
+	
+	isInvalidPhone: function(phone, flashElement) {
+		if (phone.length === 0) return;
+		const phoneNumberRegex = /^\d{3}-\d{3}-\d{4}$/;
+		if (!phoneNumberRegex.test(phone)) {
+			flashElement.textContent = 'Phone number should be formatted: 123-456-7890';
+			return true;
+		}
+	},
+	
+	removeSubmitErrorMessage: function() {
+		const messageElement = document.getElementById('submit-error');
+		if (messageElement) messageElement.remove();
+	},
+	
+	validateAllInputs: function() {
+		const inputs = document.querySelectorAll('input');
+		const validationMethods = [this.isEmptyInput.bind(this), this.isEmptyInput.bind(this), this.isInvalidEmail.bind(this), this.isInvalidPassword.bind(this), this.isInvalidPhone.bind(this)];
+		let invalidInputFound = false;
+	
+		inputs.forEach((input, index) => {
+			const flashId = input.id + '-flash';
+			const flashElement = document.getElementById(flashId);
+			
+			if (validationMethods[index](input.value, flashElement)) {
+				invalidInputFound = true;
+			}
+		});
+	
+		return invalidInputFound;
+	},
+
+	addFocusinHandler() {
+		this.form.addEventListener('focusin', (e) => {
+			if (e.target.tagName === 'BUTTON') return;
+			const targetElement = e.target;
+			const flashId = targetElement.id + '-flash'
+			const flashElement = document.getElementById(flashId);
+			flashElement.textContent = '';
+		});
+	},
+
+	addFocusoutHandler: function() {
+		this.form.addEventListener('focusout', (e) => {
+			if (e.target.tagName === 'BUTTON') return;
+			const targetElement = e.target;
+			const targetInput = targetElement.value.trim();
+			const flashId = targetElement.id + '-flash';
+			const flashElement = document.getElementById(flashId);
+	
+			if (this.isEmptyInput(targetInput, flashElement)) return;
+	
+			switch (targetElement.id) {
+				case 'email':
+					this.isInvalidEmail(targetInput, flashElement);
+					break;
+				case 'password':
+					this.isInvalidPassword(targetInput, flashElement);
+					break;
+				case 'phone':
+					this.isInvalidPhone(targetInput, flashElement);
+					break;
+			}
+		});
+	},
+
+	addSubmitHandler() {
+		this.form.addEventListener('submit', (e) => {
+			e.preventDefault();
+			const invalidInputFound = this.validateAllInputs();
+	
+			if (invalidInputFound) {
+				this.removeSubmitErrorMessage();
+				const paragraph = document.createElement('p');
+				paragraph.classList.add('flash');
+				paragraph.id = 'submit-error';
+				paragraph.textContent = 'Form cannot be submitted until errors are corrected.';
+				const firstChild = this.form.firstChild;
+				this.form.insertBefore(paragraph, firstChild);
+			} else {
+				this.removeSubmitErrorMessage();
+				this.clearForm();
+				alert('Form submitted!');
+			}
+		});
+	},
+
+	addHandlers: function() {
+		this.addFocusinHandler.call(this, this.form);
+		this.addFocusoutHandler.call(this, this.form);
+		this.addSubmitHandler.call(this, this.form);
+	},
+
+	init: function() {
+		this.form = document.querySelector('form');
+		this.addHandlers();
+	},
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-	const form = document.querySelector('form');
-
-	form.addEventListener('focusin', (e) => {
-		if (e.target.tagName === 'BUTTON') return;
-		const targetElement = e.target;
-		const flashId = targetElement.id + '-flash'
-		const flashElement = document.getElementById(flashId);
-		flashElement.textContent = '';
-	});
-
-	form.addEventListener('focusout', (e) => {
-		if (e.target.tagName === 'BUTTON') return;
-		const targetElement = e.target;
-		const targetInput = targetElement.value.trim();
-		const flashId = targetElement.id + '-flash';
-		const flashElement = document.getElementById(flashId);
-
-		if (isEmptyInput(targetInput, flashElement)) return;
-
-		switch (targetElement.id) {
-			case 'email':
-				isInvalidEmail(targetInput, flashElement);
-				break;
-			case 'password':
-				isInvalidPassword(targetInput, flashElement);
-				break;
-			case 'phone':
-				isInvalidPhone(targetInput, flashElement);
-				break;
-		}
-	});
-
-	form.addEventListener('submit', (e) => {
-		e.preventDefault();
-		const invalidInputFound = validateAllInputs();
-
-		if (invalidInputFound) {
-			removeSubmitErrorMessage();
-			const paragraph = document.createElement('p');
-			paragraph.classList.add('flash');
-			paragraph.id = 'submit-error';
-			paragraph.textContent = 'Form cannot be submitted until errors are corrected.';
-			const firstChild = form.firstChild;
-			form.insertBefore(paragraph, firstChild);
-		} else {
-			removeSubmitErrorMessage();
-			alert('Form submitted!');
-		}
-	})
+	const formValitadorObj = Object.create(FormValidator).init();
 });
